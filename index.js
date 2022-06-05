55e5f8b1fd5932d34023b84d5a2317c619e65e07
ddorest <ddodivw@gmail.com>
2022-06-05 23:43:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
-
+const sorter = (aNama, fnSort) => {
+  let aNamaBaru = [];
+  let sortingNama = fnSort(aNama);
+  for (let i = 0; i < sortingNama.length; i++) {
+    const tempNama = `${i + 1}. ${sortingNama[i]}`;
+    aNamaBaru.push(tempNama);
+  }
+  return aNamaBaru;
+}
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (aNama) => {
+  return aNama.sort();
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (aNama) => {
+  return aNama.sort().reverse();
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
